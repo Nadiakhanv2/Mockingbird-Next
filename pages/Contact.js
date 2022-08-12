709164cb19c97176ae29e31ec42f8181fa2b9cd6
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-12 21:06:04 +0500

    Add a style jsx

--- pages/Contact.js
+++ pages/Contact.js
@@ -2,10 +2,23 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      contact page
-    </div>
-  )
+    <>
+      <style jsx>
+        {`
+          .title-b {
+            color: red;
+            font-size: 50px;
+            font-family: "Arial";
+            text-align: center;
+            margin: 150px;
+          }
+        `}
+      </style>
+      <div>
+        <span className="title-b">This is contact page</span>
+      </div>
+    </>
+  );
 }
 
 export default Contact
