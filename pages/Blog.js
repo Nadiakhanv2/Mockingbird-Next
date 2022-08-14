febc276f17ec68ba557a15cd510183eeccab1295
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-14 13:41:59 +0500

    .

--- pages/Blog.js
+++ pages/Blog.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import styles from "../styles/blog.module.css";
+import Link from "next/link";
+
+
 
 const Blog = () => {
   return (
-    <div>
-      blog page
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h2>Latest Blogs</h2>
+        <div className={styles.blogItem}>
+          <Link href={"/blogpost/learn-javascript"}>
+            <h3>How to learn JavaScript in 2k22</h3>
+          </Link>
+          <p>
+            JavaScript is a programming language that allows you to write code
+            that can be run on a web browser.
+          </p>
+        </div>
+        <div className={styles.blogItem}>
+          <h3>How to learn JavaScript in 2k22</h3>
+          <p>
+            JavaScript is a programming language that allows you to write code
+            that can be run on a web browser.
+          </p>
+        </div>
+        <div className={styles.blogItem}>
+          <h3>How to learn JavaScript in 2k22</h3>
+          <p>
+            JavaScript is a programming language that allows you to write code
+            that can be run on a web browser.
+          </p>
+        </div>
+        
+      </main>
     </div>
-  )
+  );
 }
 
 export default Blog
