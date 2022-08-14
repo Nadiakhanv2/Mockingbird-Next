7dcea4bac679c12183b479fd04ae68943f98001f
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-14 13:42:23 +0500

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
-import Navbar from '../components/Navbar.js';
 import '../styles/globals.css'
+import Navbar from './Navbar';
 
 function MyApp({ Component, pageProps }) {
-  return
+  return(
   <>
     <Navbar/>
     <Component {...pageProps} />
-  </>;
+    </>
+);
 }
 
 export default MyApp
