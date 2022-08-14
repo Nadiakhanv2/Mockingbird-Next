93182ea2ac5e0572136e5730fa88db095594db0f
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-14 13:42:47 +0500

    slug

--- pages/blogpost/[Slug].js
+++ pages/blogpost/[Slug].js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React from "react";
 import { useRouter } from "next/router";
+import styles from "../../styles/BlogPost.module.css"; 
 
-const Slug = () => {
-    const router = useRouter();
-    const { slug } = router.query;
+
+const slug = () => {
+  const router = useRouter();
+  const { slug } = router.query;
   return (
-    <div>
-      {slug}
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Title of the page {slug}</h1>
+        <hr />
+        <div>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+          nulla repudiandae sint facilis, sunt corrupti numquam id illo. Ut
+          deserunt animi iste voluptatum! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Repellendus maxime rem earum repudiandae, cum
+          possimus quae assumenda nulla culpa. Odit architecto repellendus non,
+          unde recusandae placeat nisi perferendis quod nesciunt! Dolorum
+          sapiente et sint consequuntur earum blanditiis iusto reprehenderit
+          molestiae quia eligendi? Exercitationem, officia nobis!
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Slug;
+export default slug;
